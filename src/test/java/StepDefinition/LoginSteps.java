68996234b275a01509976f69c2586b5e1f403cc7
“SujitAmbore” <asujitambore@gmail.com>
2022-09-17 15:14:43 +0800

    major updates

--- src/test/java/StepDefinition/LoginSteps.java
+++ src/test/java/StepDefinition/LoginSteps.java
@@ -1,37 +1,36 @@
 package StepDefinition;
 
+import io.cucumber.java.After;
 import io.cucumber.java.en.*;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.util.concurrent.TimeUnit;
-
 public class LoginSteps {
 
-    WebDriver driver = null;
     String baseURL = "https://testpages.herokuapp.com/styled/index.html";
+    public WebDriver driver;
 
-    @Given("when user is on login page")
-    public void when_user_is_on_login_page() {
-
-        System.setProperty("webdriver.chrome.driver","/Users/sujit.ambore/Desktop/Sujit/Learnings/Selenium/src/test/java/Resources/chromedriver");
+    @Given("launch the browser")
+    public void launch_the_browser() {
+        WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-        driver.manage().window().maximize();
-        driver.get(baseURL);
+        driver.navigate().to(baseURL);
+    }
 
-        System.out.println("Given");
+    @Then("verify the page Title")
+    public void verify_title() {
+        String actualTitle = driver.getTitle();
+        System.out.println(actualTitle);
+
+        Assert.assertEquals("Selenium Test Pages", actualTitle);
     }
-    @When("verify the title of the page")
-    public void verify_the_title_of_the_page() {
-        System.out.println("When");
-    }
-    @And("enter username and password")
-    public void enter_username_and_password() {
-        System.out.println("And");
-    }
-    @Then("click on login button")
-    public void click_on_login_button() {
-        System.out.println("Then");
+
+
+    @After()
+    public void close_Browser(){
+        driver.close();
+        driver.quit();
     }
 }
---
+++ src/test/java/features/login.feature
@@ -0,0 +1,7 @@
+Feature: To test login functionality of the application
+
+  Background: Login page
+
+  Scenario: To test login page
+    Given launch the browser
+    And verify the page Title
