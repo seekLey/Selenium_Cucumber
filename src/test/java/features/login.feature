9f94239a43c49e0ad4b98608391c9ddbd047812e
“SujitAmbore” <asujitambore@gmail.com>
2023-01-19 11:23:22 +0800

    major changes

--- src/test/java/features/login.feature
+++ src/test/java/features/login.feature
@@ -1,12 +1,12 @@
 Feature: Automation Demo Site
 
-  Background: Automation Test application
+  Background: Automated HTML Form Filling
 
   Scenario: To test login page and Page title
     Given launch the browser
     And verify the page Title
 
-  Scenario Outline: To test HTML Form
+  Scenario Outline: Enter Data in HTML form and Upload File
     Given search for the HTML Form
     Then click on the Form link and verify title
     And enter username <Username>
@@ -15,5 +15,22 @@ Feature: Automation Demo Site
     And upload file
 
     Examples:
-      | Username | Password    | TextArea   |
-      | "Shawn"  | "Shawn@123" | "Comment1" |
+      | Username | Password    | TextArea            |
+      | "Shawn"  | "Shawn@123" | "Text Area Comment" |
+
+  Scenario: To test the Checkbox and Radio button selection
+    Given verify the page Title
+    Then click on the Form link and verify title
+    Then user is on HTML Form page
+    Then select Checkbox 1, Checkbox 2 and Checkbox 3 and verify selection
+    Then unselect Checkbox 1 and verify de-selection
+
+
+
+#    Then select a Radio 1 button and verify Radio button 2 and 3
+#    And select Multiple Selection <Values>
+#    And Select a value from <Dropdown>
+
+#    Examples:
+#      | Values      | Dropdown           |
+#      | "Selection" | "Drop Down Item 3" |
--- src/test/java/stepDefinition/LoginSteps.java
+++ src/test/java/stepDefinition/LoginSteps.java
@@ -34,9 +34,10 @@ public class LoginSteps {
     @Then("verify the page Title")
     public void verify_title() {
         String actualTitle = driver.getTitle();
-//        System.out.println(actualTitle);
+        System.out.println(actualTitle);
         Assert.assertEquals("Selenium Test Pages", actualTitle);
     }
+
 
     @Given("search for the HTML Form")
     public void search_HTML_Form() {
@@ -50,7 +51,9 @@ public class LoginSteps {
 
     @Then("click on the Form link and verify title")
     public void click_Form_Link() {
+        driver.findElement(By.xpath(".//*[contains(text(),'HTML Form Example')]")).isDisplayed();
         driver.findElement(By.xpath(".//*[contains(text(),'HTML Form Example')]")).click();
+
         String actualPagetitle = driver.getTitle();
         System.out.println(actualPagetitle);
 
@@ -75,9 +78,44 @@ public class LoginSteps {
     @And("upload file")
     public void select_file() {
         WebElement e = driver.findElement(By.xpath("//*[@id=\"HTMLFormElements\"]/table/tbody/tr[4]/td/input[1]"));
-
         e.sendKeys("/Users/sujit.ambore/Desktop/sample1.png");
         driver.findElement(By.xpath(".//*[@type='checkbox' and @value='cb1']")).click();
+    }
+
+    @Given("user is on HTML Form page")
+    public void verify_page() {
+        String actualPagetitle = driver.getTitle();
+        System.out.println(actualPagetitle);
+        Assert.assertEquals("HTML Form Elements", actualPagetitle);
+    }
+
+    @Then("select Checkbox 1, Checkbox 2 and Checkbox 3 and verify selection")
+    public void checkBox_selection() {
+        driver.findElement(By.xpath(".//*[@name='checkboxes[]' and @value ='cb1']")).isDisplayed();
+        driver.findElement(By.xpath(".//*[@name='checkboxes[]' and @value ='cb1']")).click();
+        driver.findElement(By.xpath(".//*[@name='checkboxes[]' and @value ='cb1']")).isSelected();
+
+        driver.findElement(By.xpath(".//*[@name='checkboxes[]' and @value ='cb2']")).click();
+        boolean c2 = driver.findElement(By.xpath(".//*[@name='checkboxes[]' and @value ='cb2']")).isSelected();
+        Assert.assertTrue(c2);
+
+        boolean c3 = driver.findElement(By.xpath(".//*[@name='checkboxes[]' and @value ='cb3']")).isSelected();
+        Assert.assertTrue(c3);
+    }
+
+    @Then("unselect Checkbox 1 and verify de-selection")
+    public void uncheck_Box() {
+        driver.findElement(By.xpath(".//*[@name='checkboxes[]' and @value ='cb1']")).isDisplayed();
+        driver.findElement(By.xpath(".//*[@name='checkboxes[]' and @value ='cb1']")).click();
+        driver.findElement(By.xpath(".//*[@name='checkboxes[]' and @value ='cb1']")).isSelected();
+        driver.findElement(By.xpath(".//*[@name='checkboxes[]' and @value ='cb1']")).click();
+
+        WebElement checkbox = driver.findElement(By.xpath(".//*[@name='checkboxes[]' and @value ='cb1']"));
+        if (!checkbox.isSelected()) {
+            System.out.println("FAIL");
+        } else {
+            System.out.println("PASS");
+        }
     }
 
     @After()
